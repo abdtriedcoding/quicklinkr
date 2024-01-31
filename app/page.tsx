2d5cf564bec1a1d3969e3cbacfd769c758f78d36
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-31 23:04:21 +0530

    working on auth using nextauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/components/ui/button"
+import { authOptions } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  console.log(session)
   return (
     <div>
       <Button>Click me</Button>
     </div>
-  )
+  );
 }

23f1a8d372e34d7cd26f6406b4e22209aff66e46
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-02-01 23:52:17 +0530

    add adapter to sync auth with database

--- auth.ts
+++ auth.ts
@@ -1,13 +1,16 @@
-import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { adminDb } from "./firebase/firebase-admin";
 
-export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
+export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    // ...add more providers here
   ],
-} satisfies NextAuthOptions;
+  session: {
+    strategy: "jwt",
+  },
+  adapter: FirestoreAdapter(adminDb),
+};

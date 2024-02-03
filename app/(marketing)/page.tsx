5dbe114b23d45437a676efbbac031cd35774f8d2
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-02-03 23:55:46 +0530

    add action to fetch current user from firebase

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,8 +1,12 @@
 import BrandingText from "@/app/(marketing)/_components/branding-text";
 import BrandingImage from "@/app/(marketing)/_components/branding-image";
 import FeatureSection from "@/app/(marketing)/_components/feature-section";
+import getCurrentUser from "../actions/getCurrentUser";
 
-const MarketingPage = () => {
+const MarketingPage = async () => {
+  const user = await getCurrentUser();
+  console.log(user);
+
   return (
     <>
       <BrandingText />
---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,35 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { db } from "@/firebase/firebase-config";
+import { doc, getDoc } from "firebase/firestore";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.id) {
+      return null;
+    }
+
+    const userDocRef = doc(db, `users/${session.user.id}`);
+    const userDocSnapshot = await getDoc(userDocRef);
+
+    if (!userDocSnapshot.exists()) {
+      return null;
+    }
+
+    const currentUser = userDocSnapshot.data();
+
+    return {
+      ...currentUser,
+      uid: session?.user?.id,
+    };
+  } catch (error) {
+    console.error("Error fetching current user:", error);
+    return null;
+  }
+}

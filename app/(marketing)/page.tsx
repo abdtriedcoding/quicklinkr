ddfd90b4836872942daa30744c40e5996297bb7f
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-02-03 23:36:36 +0530

    refactor code by adding marketing page

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,15 @@
+import BrandingText from "@/app/(marketing)/_components/branding-text";
+import BrandingImage from "@/app/(marketing)/_components/branding-image";
+import FeatureSection from "@/app/(marketing)/_components/feature-section";
+
+const MarketingPage = () => {
+  return (
+    <>
+      <BrandingText />
+      <BrandingImage />
+      <FeatureSection />
+    </>
+  );
+};
+
+export default MarketingPage;

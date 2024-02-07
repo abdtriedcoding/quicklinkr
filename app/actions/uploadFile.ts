b23f74408e2837a3ddeda1e02441ff513ab6c8d5
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-02-08 01:04:31 +0530

    refactor upload files code

---
+++ app/actions/uploadFile.ts
@@ -0,0 +1,47 @@
+import { db, storage } from "@/firebase/firebase-config";
+import {
+  writeBatch,
+  doc,
+  getDoc,
+  serverTimestamp,
+  collection,
+} from "firebase/firestore";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { User } from "next-auth";
+
+export const uploadFile = async (selectedFile: File, user: User) => {
+  const batch = writeBatch(db);
+  try {
+    if (!user) return;
+    const userRef = doc(db, `users/${user.id}`);
+    const userDoc = await getDoc(userRef);
+
+    // Calculate the new storageUsed by adding the size of the selected file
+    const storageUsed = userDoc?.data()?.storageUsed;
+    const newSize =
+      storageUsed !== undefined
+        ? storageUsed + selectedFile.size
+        : selectedFile.size;
+
+    const newFileData = {
+      filename: selectedFile.name,
+      createdAt: serverTimestamp(),
+      type: selectedFile.type,
+      size: selectedFile.size,
+      downloadURL: null,
+    };
+
+    const docRef = doc(collection(db, "users", user.id, "files"));
+    batch.set(docRef, newFileData);
+    const imageRef = ref(storage, `users/${user.id}/files/${docRef.id}`);
+    await uploadBytes(imageRef, selectedFile);
+    const downloadURL = await getDownloadURL(imageRef);
+    batch.update(docRef, { downloadURL });
+
+    batch.update(userRef, { storageUsed: newSize });
+    await batch.commit();
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
+};
--- components/image-dropzone.tsx
+++ components/image-dropzone.tsx
@@ -1,96 +1,50 @@
 "use client";
 
-import { db, storage } from "@/firebase/firebase-config";
-import { cn } from "@/lib/utils";
-import {
-  collection,
-  doc,
-  getDoc,
-  serverTimestamp,
-  writeBatch,
-} from "firebase/firestore";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { useSession } from "next-auth/react";
-import { useState } from "react";
-import DropzoneComponent from "react-dropzone";
 import toast from "react-hot-toast";
+import DropzoneComponent from "react-dropzone";
+
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { uploadFile } from "@/app/actions/uploadFile";
 
 export default function ImageDropzone() {
   const { data: session } = useSession();
   const [loading, setLoading] = useState(false);
-  const maxSize = 20971520; //20MB max
 
   const onDrop = async (acceptedFile: File[]) => {
-    acceptedFile.forEach((file) => {
-      const reader = new FileReader();
-      reader.onabort = () => console.log("file reading was aborted");
-      reader.onerror = () => console.log("file reading has failed");
-
-      reader.onload = async () => {
-        await uploadFile(file);
-      };
-      reader.readAsArrayBuffer(file);
-    });
+    try {
+      setLoading(true);
+      const toastId = toast.loading("Uploading...");
+      await Promise.all(
+        acceptedFile.map(async (file) => {
+          await handleFileUpload(file);
+        })
+      );
+      toast.success("Uploaded successfully", {
+        id: toastId,
+      });
+    } catch (error) {
+      console.error("Error occurred while uploading:", error);
+      toast.error("An error occurred while uploading. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
   };
 
-  const uploadFile = async (selectedFile: File) => {
+  const handleFileUpload = async (selectedFile: File) => {
     if (!session?.user || loading) return;
-    setLoading(true);
-    const toastId = toast.loading("uploading...");
-    const batch = writeBatch(db);
-
-    const userRef = doc(db, "users", session.user.id);
-    const userDoc = await getDoc(userRef);
-
-    // Calculate the new storageUsed by adding the size of the selected file
-    const storageUsed = userDoc?.data()?.storageUsed;
-    const newSize =
-      storageUsed !== undefined
-        ? storageUsed + selectedFile.size
-        : selectedFile.size;
-
-    const newFileData = {
-      filename: selectedFile.name,
-      createdAt: serverTimestamp(),
-      type: selectedFile.type,
-      size: selectedFile.size,
-      downloadURL: null,
-    };
-
-    const docRef = doc(collection(db, "users", session.user.id, "files"));
-    batch.set(docRef, newFileData);
-    const imageRef = ref(
-      storage,
-      `users/${session.user.id}/files/${docRef.id}`
-    );
-    await uploadBytes(imageRef, selectedFile);
-    const downloadURL = await getDownloadURL(imageRef);
-    batch.update(docRef, { downloadURL });
-
-    batch.update(userRef, { storageUsed: newSize });
-    await batch.commit();
-    toast.success("Uploaded successfully", {
-      id: toastId,
-    });
-    setLoading(false);
+    try {
+      await uploadFile(selectedFile, session.user);
+    } catch (error) {
+      console.error("Error occurred while uploading file:", error);
+      throw error;
+    }
   };
 
   return (
-    <DropzoneComponent
-      disabled={loading}
-      minSize={0}
-      maxSize={maxSize}
-      onDrop={onDrop}
-    >
-      {({
-        getRootProps,
-        getInputProps,
-        isDragActive,
-        isDragReject,
-        fileRejections,
-      }) => {
-        const isFileTooLarge =
-          fileRejections.length > 0 && fileRejections[0].file.size > maxSize;
+    <DropzoneComponent disabled={loading} onDrop={onDrop}>
+      {({ getRootProps, getInputProps, isDragActive, isDragReject }) => {
         return (
           <>
             <div
@@ -108,11 +62,6 @@ export default function ImageDropzone() {
               {isDragActive && !isDragReject && "Drop to upload this file!"}
               {isDragReject && "File type not accepted, sorry!!"}
             </div>
-            {isFileTooLarge && (
-              <div className="text-red-500 text-center mt-2">
-                File is too large
-              </div>
-            )}
           </>
         );
       }}

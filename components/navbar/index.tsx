42de3580aa512b50a3229af78ed4b0fd0949e596
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-02-09 00:20:46 +0530

    fix the bug in progress bar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import Link from "next/link";
+import prettyBytes from "pretty-bytes";
 import { useSession } from "next-auth/react";
+import { db } from "@/firebase/firebase-config";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { UserProps } from "@/types";
+import { buttonVariants } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import { Progress } from "@/components/ui/progress";
 
 import UserMenu from "@/components/user-menu";
 import SignIn from "@/components/navbar/signin";
 import GetStarted from "@/components/navbar/get-started";
-import { buttonVariants } from "@/components/ui/button";
-import { usePathname } from "next/navigation";
-import { Progress } from "@/components/ui/progress";
-import { useEffect, useState } from "react";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "@/firebase/firebase-config";
-import { UserProps } from "@/types";
-import prettyBytes from "pretty-bytes";
 
 const Navbar = () => {
   const { data: session } = useSession();
@@ -32,7 +32,7 @@ const Navbar = () => {
     getUser();
   }, [session]);
 
-  const storageUsedPercentage = (user?.storageUsed ?? 0 / (1 * 1024 * 1024 * 1024)) * 100;
+  const storageUsedPercentage = (user?.storageUsed ?? 0) / (1 * 1024 * 1024);
 
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
@@ -60,7 +60,7 @@ const Navbar = () => {
             ) : (
               <>
                 <div className="flex flex-col items-center space-y-1 text-xs whitespace-nowrap">
-                  <p>{prettyBytes(user?.storageUsed ?? 0)} / 1 GB</p>
+                  <p>{prettyBytes(user?.storageUsed ?? 0)} / 100 MB</p>
                   <Progress value={storageUsedPercentage} />
                 </div>
                 {pathname !== "/dashboard" && (
